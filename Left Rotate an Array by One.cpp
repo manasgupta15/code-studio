71c148b8c24d01e75f73363b71c1f19789185bbf
MANAS GUPTA <120665112+manasgupta15@users.noreply.github.com>
2024-01-23 11:01:41 +0530

    Create Left Rotate an Array by One.cpp

---
+++ Left Rotate an Array by One.cpp
@@ -0,0 +1,12 @@
+https://www.codingninjas.com/studio/problems/left-rotate-an-array-by-one_5026278?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM
+
+#include <bits/stdc++.h> 
+vector<int> rotateArray(vector<int>& arr, int n) {
+    // Write your code here.
+    int temp = arr[0];
+    for(int i=1;i<n;i++){
+        arr[i-1] = arr[i];
+    }
+    arr[n-1] = temp;
+    return arr;
+}
