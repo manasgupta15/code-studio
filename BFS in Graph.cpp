8a388e536cfbc42fa2a356520d5f05ed181127b1
MANAS GUPTA <120665112+manasgupta15@users.noreply.github.com>
2024-02-25 19:11:55 +0530

    Create  BFS in Graph.cpp

---
+++ BFS in Graph.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+vector<int> bfsTraversal(int n, vector<vector<int>> &adj){
+    // Write your code here.
+    int vis[n] = {0};
+    vis[0] = 1;
+    queue<int>q;
+    q.push(0);
+    vector<int> bfs;
+    while(!q.empty()) {
+        int node = q.front();
+        q.pop();
+        bfs.push_back(node);
+
+        for(auto it : adj[node]) {
+            if(!vis[it]) {
+                vis[it] = 1;
+                q.push(it);
+            }
+        }
+    }
+    return bfs; 
+}
