8283ac336bd9db2e84fc139e5ea8c395aeafb51d
MANAS GUPTA <120665112+manasgupta15@users.noreply.github.com>
2024-03-24 11:12:25 +0530

    Create Insert At The Front of a Doubly Linked List.cpp

---
+++ Insert At The Front of a Doubly Linked List.cpp
@@ -0,0 +1,21 @@
+/**
+ * Definition of doubly linked list:
+ *
+ * struct Node {
+ *      int value;
+ *      Node *prev;
+ *      Node *next;
+ *      Node() : value(0), prev(nullptr), next(nullptr) {}
+ *      Node(int val) : value(val), prev(nullptr), next(nullptr) {}
+ *      Node(int val, Node *p, Node *n) : value(val), prev(p), next(n) {}
+ * };
+ *
+ *************************************************************************/
+
+Node* insertAtFront(Node *head, int k) {
+    Node *newHead = new Node(k, nullptr, head);
+    if (head != nullptr) {
+        head->prev = newHead;
+    }
+    return newHead;
+}
